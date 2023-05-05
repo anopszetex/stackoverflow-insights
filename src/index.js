6f4c7636570e97bd050b854a8d2f430f740c622d
Andre Luis <andre.1357.luis@gmail.com>
2023-05-05 12:56:18 -0300

    🚧 prepare stream

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import b from './teste/b.js';
+import { readdir } from 'node:fs/promises';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
 
-console.log('b', b);
+const folder = './docs/state-of-js';
+
+async function prepareStream(folder) {
+  const files = await readdir(folder);
+
+  const streams = files.map(file => {
+    return createReadStream(path.join(folder, file));
+  });
+
+  console.log(streams);
+}
+
+await prepareStream(folder);
