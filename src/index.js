d90fd2bf88d19ab34abeb621997d42d375fae972
Andre Luis <andre.1357.luis@gmail.com>
2023-05-05 23:25:43 -0300

    ✨ stream-concat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import { readdir } from 'node:fs/promises';
-import { createReadStream } from 'node:fs';
+import { readdir, stat } from 'node:fs/promises';
+import { PassThrough } from 'node:stream';
 import path from 'node:path';
+import fs from 'node:fs';
 
-const folder = './docs/state-of-js';
+const FOLDER = './docs/state-of-js';
 
+/**
+ *
+ * @param {import('node:fs').ReadStream[]} streams - array of streams to be concatenated
+ * @returns {import('node:stream').PassThrough}
+ */
+function concatStream(streams) {
+  return streams.reduce((acc, current, _, items) => {
+    current.pipe(acc, { end: false });
+
+    current.on('end', () => {
+      return items.every(readStream => readStream.ended) && acc.end();
+    });
+
+    return acc;
+  }, new PassThrough());
+}
+
+/**
+ *
+ * @param {string} folder - folder to be read
+ */
 async function prepareStream(folder) {
   const files = await readdir(folder);
 
   const streams = files.map(file => {
-    return createReadStream(path.join(folder, file));
+    return fs.createReadStream(path.join(folder, file));
   });
 
-  console.log(streams);
+  const stream = concatStream(streams);
+
+  return { stream };
 }
 
-await prepareStream(folder);
+await prepareStream(FOLDER);
