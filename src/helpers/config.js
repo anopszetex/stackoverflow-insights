bea27ca937d8f614feb5766bf4544eed4e67eaec
Andre Luis <andre.1357.luis@gmail.com>
2023-05-07 20:56:23 -0300

    ✨ Add config to mapper list

---
+++ src/helpers/config.js
@@ -0,0 +1,54 @@
+const LIKES = ['interested', 'would_use'];
+
+const YEARS = ['2016', '2017', '2018', '2019'];
+
+function defaultY() {
+  return [0, 0, 0, 0];
+}
+
+const lineChartData = {
+  angular: {
+    title: 'angular',
+    x: YEARS,
+    y: defaultY(),
+    style: {
+      // vermelho angular
+      line: [170, 42, 44],
+    },
+  },
+  react: {
+    title: 'react',
+    x: YEARS,
+    y: defaultY(),
+    // azul react
+    style: { line: [97, 218, 251] },
+  },
+  vuejs: {
+    title: 'vuejs',
+    x: YEARS,
+    y: defaultY(),
+    // verde vue
+    style: { line: [63, 178, 127] },
+  },
+  ember: {
+    title: 'ember',
+    x: YEARS,
+    y: defaultY(),
+    // laranja vue
+    style: { line: [218, 89, 46] },
+  },
+  backbone: {
+    title: 'backbone',
+    x: YEARS,
+    y: defaultY(),
+    // verde backbone
+    style: { line: [37, 108, 74] },
+  },
+};
+
+export default {
+  lineChartData,
+  likes: LIKES,
+  years: YEARS,
+  tecnologiesInAnalysis: Reflect.ownKeys(lineChartData),
+};
